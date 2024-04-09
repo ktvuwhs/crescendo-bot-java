c774a281666a9fd1e26e17e4302c2b16be18f993
ktvuwhs <ktvu@hbuhsd.edu>
2024-04-08 19:09:26 -0700

    added wheelbase and tracklength

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,14 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.*;
+
 public final class Constants {
   public static final int kControllerPort = 0;
 
   public final class DriveConstants {
     public static final int kDrivePort = 0;
     public static final int kTurnPort = 1;
+    public static final double kWheelbase = Inches.of(24).in(Meters);
+    public static final double kTracklength = Inches.of(24).in(Meters);
   }
 }
