4db00673da13994fa622443e20cf2788ddad7f66
ktvuwhs <ktvu@hbuhsd.edu>
2024-04-08 18:50:26 -0700

    testing initial code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,26 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
+import static frc.robot.Constants.*;
+import frc.robot.subsystems.Drivebase;
+
 public class RobotContainer {
+  private final Drivebase m_drivebase = new Drivebase();
+
   public RobotContainer() {
     configureBindings();
+
+    m_drivebase.setDefaultCommand(m_drivebase.offCommand());
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    final CommandXboxController m_controller = new CommandXboxController(kControllerPort);
+    m_controller.a().whileTrue(m_drivebase.driveSpeedCommand(0.5));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
