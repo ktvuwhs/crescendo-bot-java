17ad48491e5e43dd5151a01719e16f2a3d272e7f
ktvuwhs <ktvu@hbuhsd.edu>
2024-04-08 18:50:12 -0700

    created file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public final class Constants {
+  public static final int kControllerPort = 0;
+
+  public final class DriveConstants {
+    public static final int kDrivePort = 0;
+    public static final int kTurnPort = 1;
+  }
+}
