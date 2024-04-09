82d50ad77f8e2aa99bb69435a76b3e374f70e77f
ktvuwhs <ktvu@hbuhsd.edu>
2024-04-08 19:09:55 -0700

    removed initial code

--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -4,54 +4,30 @@
 
 package frc.robot.subsystems;
 
-// CTRE Libraries
-import com.ctre.phoenix6.hardware.TalonFX;
-
-// REV Libraries
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
-
 // WPILib
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+
+// import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.DriveConstants.*;
 
 public class Drivebase extends SubsystemBase {
-  private final TalonFX m_driveMotor;
-  private final CANSparkMax m_turnMotor;
-
+  private final SwerveDriveKinematics m_swerveKinematics;
   /** Creates a new Drivebase. */
   public Drivebase() {
-    m_driveMotor = new TalonFX(kDrivePort, "rio");
-    m_turnMotor = new CANSparkMax(kTurnPort, MotorType.kBrushless);
-  }
-
-  private void setDriveSpeed(final double speed) {
-    m_driveMotor.set(speed);
-  }
-
-  private void setTurnSpeed(final double speed) {
-    m_turnMotor.set(speed);
+    m_swerveKinematics = new SwerveDriveKinematics(
+      new Translation2d(kWheelbase / 2, kTracklength / 2),  // top-left
+      new Translation2d(kWheelbase / 2, -kTracklength / 2), // top-right
+      new Translation2d(-kWheelbase / 2, kTracklength / 2), // bot-left
+      new Translation2d(-kWheelbase / 2, -kTracklength / 2) // bot-right
+    );
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-
-  public Command driveSpeedCommand(final double speed) {
-    return this.run(() -> setDriveSpeed(speed));
-  }
-  
-  public Command turnSpeedCommand(final double speed) {
-    return this.run(() -> setTurnSpeed(speed));
-  }
-
-  public Command offCommand() {
-    return this.runOnce(() -> {
-      setDriveSpeed(0);
-      setTurnSpeed(0);
-    });
   }
 }
